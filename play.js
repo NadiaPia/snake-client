416bc6e14628d0ddcb141cf30faed31a8d047dd7
NadiaPia <206i417@gmail.com>
2022-06-16 04:42:05 +0000

    Implement inpute module

--- play.js
+++ play.js
@@ -1,26 +1,10 @@
 
 const {connect} = require('./client.js');
+const {setupInput} = require('./input.js');
+
 console.log("Connecting ...");
+
 connect();
-
-//setup interface to handle user input from stdin
-
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-
-  const handleUserInput = function(key) {
-    if (key === '\u0003') {
-      process.exit();
-    }
-  };
-
-  stdin.on("data", handleUserInput);
-  return stdin;
-
-};
 setupInput();
-//console.log("this is stdin", setupInput())
+
 
